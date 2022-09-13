55d0cfc9859451fa4a3fe79add2d6602a0c8d667
Szpoti <balage.farago@gmail.com>
2022-09-13 14:28:25 +0200

    Add script to run

---
+++ Dumplings.Cli/RunAndSendDumplings.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+FILE1='LastProcessedBlockHeight.txt'
+FILE2='OtherCoinJoins.txt'
+FILE3='SamouraiPostMixTxs.txt'
+FILE4='OtherCoinJoinPostMixTxs.txt'
+FILE5='SamouraiCoinJoins.txt'
+FILE6='SamouraiTx0s.txt'
+FILE7='WabiSabiCoinJoins.txt'
+FILE8='WasabiPostMixTxs.txt'
+FILE9='WasabiCoinJoins.txt'
+
+# Make sure to have 'Config.txt' in the same file as this script.
+# The following variables need to be imported from the config file:
+
+# HOST = FTP server address,
+# FTPUSER & FTPPASSWD = FTP login credentials,
+# RPCUSER & RPCPASSWD = RPC credentials for Bitcoin Kntos,
+# OLDCORDXPUB(two keys, divided by ',') & WABISABIXPUB = Coordinator XPUBs,
+# OUTFOLDER = Where to save processed dumplings,
+# SCANNERFOLDER = Where to look for the coinjoin stat files to send
+
+source Config.txt
+
+echo "Syncronizing blockchain"
+dotnet run -c Release -- sync --rpcuser=$RPCUSER --rpcpassword=$RPCPASSWD --nowaitonexit
+
+echo "Processing Wasabi Coordinator Stats"
+dotnet run -c Release -- WasabiCoordStats --xpub=$OLDCORDXPUB --outfolder=$OUTFOLDER --rpcuser=$RPCUSER --rpcpassword=$RPCPASSWD --nowaitonexit
+
+echo "Processing WabiSabi Coordinator Stats"
+dotnet run -c Release -- WabiSabiCoordStats --xpub=$WABISABIXPUB --outfolder=$OUTFOLDER --rpcuser=$RPCUSER --rpcpassword=$RPCPASSWD --nowaitonexit
+
+cd $SCANNERFOLDER
+
+echo "Starting FTP upload"
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $FTPUSER
+quote PASS $FTPPASSWD
+put $FILE1
+put $FILE2
+put $FILE3
+put $FILE4
+put $FILE5
+put $FILE6
+put $FILE7
+put $FILE8
+put $FILE9
+quit
+END_SCRIPT
+
+echo "Finished"
