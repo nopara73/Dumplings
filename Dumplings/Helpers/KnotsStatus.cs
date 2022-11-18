e0fafc89f982fd55d51f76a308da7c626fbf01b6
Pavol Rusnak <pavol@rusnak.io>
2022-11-18 12:47:01 +0100

    Indicate that Bitcoin Core can be used, not only Bitcoin Knots
    required getblock verbosity 3 was introduced in Bitcoin Core 23.0

--- Dumplings/Helpers/KnotsStatus.cs
+++ Dumplings/Helpers/KnotsStatus.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace Dumplings.Helpers
 {
-    public static class KnotsStatus
+    public static class BitcoinStatus
     {
         public static async Task CheckAsync(RPCClient client)
         {
-            Logger.LogInfo("Checking Bitcoin Knots sync status...");
+            Logger.LogInfo("Checking Bitcoin sync status...");
 
             var bci = await client.GetBlockchainInfoAsync();
 
             var missingBlocks = bci.Headers - bci.Blocks;
             if (missingBlocks != 0)
             {
-                throw new InvalidOperationException($"Knots is not synchronized. Blocks missing: {missingBlocks}.");
+                throw new InvalidOperationException($"Bitcoin is not synchronized. Blocks missing: {missingBlocks}.");
             }
 
-            Logger.LogInfo($"Bitcoin Knots is synchronized. Current height: {bci.Blocks}.");
+            Logger.LogInfo($"Bitcoin is synchronized. Current height: {bci.Blocks}.");
         }
     }
 }
--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 1. Get Git: https://git-scm.com/downloads
 1. Get .NET Core SDK: https://www.microsoft.com/net/download
-1. Get Bitcoin Knots: https://bitcoinknots.org/
+1. Get Bitcoin Core: https://bitcoincore.org/ or Bitcoin Knots: https://bitcoinknots.org/
 1. Make sure your bitcoin.conf has the following entries: `txindex = 1`, `server = 1`, `rpcuser = user`, `rpcpassword = password`, `listen = 1`, `rpcworkqueue = 128`
-1. Run Knots and wait until it's synchronized.
+1. Run Bitcoin and wait until it's synchronized.
 1. `git clone https://github.com/nopara73/Dumplings.git`
 1. `cd Dumplings/Dumplings.Cli`
 1. `dotnet run -- sync --rpcuser=user --rpcpassword=password`
 
 # Usage
 
-1. Run Knots, wait until it's synchronized.
+1. Run Bitcoin, wait until it's synchronized.
 1. `cd Dumplings/Dumplings.Cli`
 
 ## 1. Synchronization
