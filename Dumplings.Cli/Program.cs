38af0e9794bd500b7eb9eaf0dfdd589c611026bc
Szpoti <balage.farago@gmail.com>
2022-12-13 13:31:16 +0100

    Refactor Program

--- Dumplings.Cli/Program.cs
+++ Dumplings.Cli/Program.cs
@@ -52,22 +52,6 @@ namespace Dumplings.Cli
                 FileStream fileStream = null;
                 StreamWriter writer = null;
                 TextWriter oldOut = Console.Out;
-                if (!string.IsNullOrEmpty(outputFolder))
-                {
-                    var filePath = Path.Combine(outputFolder, $"Dump{DateTime.Now:yyMMdd_HHmmss}.txt");
-                    try
-                    {
-                        fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
-                        writer = new StreamWriter(fileStream);
-                        Console.WriteLine($"Console output redirected to: {filePath}.");
-                        Console.SetOut(writer);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Cannot open file: {filePath}.");
-                        Console.WriteLine(ex.Message);
-                    }
-                }
                 try
                 {
                     if (command == Command.Resync)
@@ -75,128 +59,127 @@ namespace Dumplings.Cli
                         var scanner = new Scanner(client);
                         await scanner.ScanAsync(rescan: true);
                     }
-                    else if (command == Command.Sync)
+                    else
                     {
                         var scanner = new Scanner(client);
                         await scanner.ScanAsync(rescan: false);
                     }
-                    else if (command == Command.Check)
+
+                    if (!string.IsNullOrEmpty(outputFolder))
                     {
-                        var loadedScannerFiles = Scanner.Load();
+                        var filePath = Path.Combine(outputFolder, $"Dump{DateTime.Now:yyMMdd_HHmmss}.txt");
+                        try
+                        {
+                            fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+                            writer = new StreamWriter(fileStream);
+                            Console.WriteLine($"Console output redirected to: {filePath}.");
+                            Console.SetOut(writer);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Cannot open file: {filePath}.");
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    var loadedScannerFiles = Scanner.Load();
+
+                    if (command == Command.Check)
+                    {
                         var checker = new Checker(loadedScannerFiles);
                         checker.Check();
                     }
                     else if (command == Command.CountCoinJoins)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateCoinJoinNumbers();
                     }
                     else if (command == Command.MonthlyVolumes)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateAndUploadMonthlyVolumes();
                     }
                     else if (command == Command.FreshBitcoins)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateAndUploadFreshBitcoins();
                     }
                     else if (command == Command.FreshBitcoinAmounts)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateFreshBitcoinAmounts();
                     }
                     else if (command == Command.FreshBitcoinsDaily)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateFreshBitcoinsDaily();
                     }
                     else if (command == Command.NeverMixed)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateNeverMixed();
                     }
                     else if (command == Command.CoinJoinEquality)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateEquality();
                     }
                     else if (command == Command.CoinJoinIncome)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateIncome();
                     }
                     else if (command == Command.PostMixConsolidation)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculatePostMixConsolidation();
                     }
                     else if (command == Command.SmallerThanMinimum)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateSmallerThanMinimumWasabiInputs();
                     }
                     else if (command == Command.MonthlyEqualVolumes)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateMonthlyEqualVolumes();
                     }
                     else if (command == Command.AverageUserCount)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateMonthlyAverageMonthlyUserCounts();
                     }
                     else if (command == Command.AverageNetworkFeePaidByUserPerCoinjoin)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateMonthlyNetworkFeePaidByUserPerCoinjoin();
                     }
                     else if (command == Command.Records)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateRecords();
                     }
                     else if (command == Command.UniqueCountPercent)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateUniqueCountPercent();
                     }
                     else if (command == Command.ListFreshBitcoins)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.ListFreshBitcoins();
                     }
                     else if (command == Command.UnspentCapacity)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateUnspentCapacity(client);
                     }
                     else if (command == Command.WasabiCoordStats)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateWasabiCoordStats(GetXpub(args));
                     }
                     else if (command == Command.WabiSabiCoordStats)
                     {
-                        var loadedScannerFiles = Scanner.Load();
                         var stat = new Statista(loadedScannerFiles, client);
                         stat.CalculateWabiSabiCoordStats(GetXpub(args));
                     }
